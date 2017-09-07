6271e1f9799cf08025cd073846a863ff08ebda97
Chris O'Brien <Chris@lawn-128-61-115-29.lawn.gatech.edu>
2017-09-07 16:29:31 -0400

    Implemented Person3.java -cob

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Chris O'Brien
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,6 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  return new StringBuilder(input).reverse().toString();
 	}
 }
